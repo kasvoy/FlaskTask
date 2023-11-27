737778a7978ac9574486a97f1944434aaa3cfa85
Wojciech <woj.kasperczyk@gmail.com>
2023-11-27 11:06:54 +0400

    fixed issues with delete modals

--- flasktask/notes_tasks.py
+++ flasktask/notes_tasks.py
@@ -22,11 +22,18 @@ def home():
         yet_done_tasks = db.session.scalars(stmt_yet_done_tasks).all()
         done_tasks     = db.session.scalars(stmt_done_tasks).all()
 
-
-    return render_template("home.html", notes=notes, yet_done_tasks=yet_done_tasks, done_tasks=done_tasks)
+    if notes and not (yet_done_tasks or done_tasks):
+        print('were here')
+        return render_template("home_notes_only.html", notes=notes)
+    
+    elif (yet_done_tasks or done_tasks) and not notes:
+        print('WEREHERE')
+        return render_template("home_tasks_only.html",yet_done_tasks=yet_done_tasks, done_tasks=done_tasks)
+    else:
+        return render_template("home.html", notes=notes, yet_done_tasks=yet_done_tasks, done_tasks=done_tasks)
 
        
-@tasks_bp.route("/note/new", methods=['GET', 'POST'])
+@tasks_bp.route("/note/new/", methods=['GET', 'POST'])
 @login_required
 def create_note():
     form = NoteForm()
@@ -46,7 +53,7 @@ def create_note():
     return render_template("notes/create_note.html", form=form, title="New note")
 
 
-@tasks_bp.route("/task/new", methods=['GET', 'POST'])
+@tasks_bp.route("/task/new/", methods=['GET', 'POST'])
 @login_required
 def create_task():
     form = TaskForm()
@@ -68,7 +75,7 @@ def create_task():
     return render_template("tasks/create_task.html", form=form)
 
 
-@tasks_bp.route("/note/<note_id>")
+@tasks_bp.route("/note/<note_id>/")
 def note_by_id(note_id):
     print(note_id)
     note = db.session.get(Note, note_id)
@@ -83,14 +90,14 @@ def note_by_id(note_id):
     return render_template("notes/note_id.html", note=note)
 
 
-@tasks_bp.route("/task/<task_id>")
+@tasks_bp.route("/task/<task_id>/")
 def task_by_id(task_id):
     task = db.session.execute(db.select(Task).where(Task.id==task_id, Task.user_id==current_user.id))
 
     return render_template("tasks/task_id.html", task=task)
 
 
-@tasks_bp.route("/task/<task_id>/switch_done")
+@tasks_bp.route("/task/<task_id>/switch_done/")
 def switch_done(task_id):
     task = db.session.get(Task, task_id)
 
@@ -104,7 +111,7 @@ def switch_done(task_id):
 
     return redirect(url_for('tasks.home'))
 
-@tasks_bp.route("/note/<note_id>/update", methods=['GET', 'POST'])
+@tasks_bp.route("/note/<note_id>/update/", methods=['GET', 'POST'])
 def update_note(note_id):
     
     note = db.session.get(Note, note_id)
@@ -129,7 +136,7 @@ def update_note(note_id):
 
     return render_template('notes/create_note.html', form=form, page_title="Update note")
 
-@tasks_bp.route("/task/<task_id>/delete")
+@tasks_bp.route("/task/<task_id>/delete/")
 def delete_task(task_id):
 
     task = db.session.get(Task, task_id)
@@ -144,7 +151,7 @@ def delete_task(task_id):
     return redirect(url_for('tasks.home'))
 
 
-@tasks_bp.route("/note/<note_id>/delete")
+@tasks_bp.route("/note/<note_id>/delete/")
 def delete_note(note_id):
 
     note = db.session.get(Note, note_id)
@@ -158,3 +165,14 @@ def delete_note(note_id):
 
 
     return redirect(url_for('tasks.home'))
+
+@tasks_bp.route("/note/<note_id>/sure/")
+def are_you_sure(note_id):
+
+    note = db.session.get(Note, note_id)
+    if not note:
+        abort(404)
+    if note.user_id != current_user.id:
+        abort(403)
+
+    return render_template('notes/sure_to_delete.html', note=note)
